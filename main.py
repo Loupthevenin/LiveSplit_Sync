c52130cc862b35c22f5975b03ccad14bb37fa70e
MrShadowsWolf <loup.thevenin.1@gmail.com>
2024-03-12 20:41:15 +0100

    reset

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from analyser import convert_time_format
 unique_delta_values = []
 unique_final_values = []
 unique_split_index_values = []
-unique_timer_phase_values = []
+unique_timer_phase_value = [""]
 
 
 def get_unique_delta_time(data):
@@ -30,6 +30,17 @@ def get_unique_final_time(data):
         return data
 
 
+def get_unique_timer_phase(data):
+    global unique_timer_phase_value
+
+    if data == "NotRunning" and unique_timer_phase_value[-1] == "Running":
+        unique_timer_phase_value.append(data)
+        return True
+    else:
+        unique_timer_phase_value.append(data)
+        return False
+
+
 def main():
     SERVER_IP = '127.168.1.27'
     PORT = 16834
@@ -38,6 +49,7 @@ def main():
     client.connect()
 
     while True:
+        # DELTA TIME
         client.send_data(getdelta)
         data = client.receive_data()
         unique_delta_time = get_unique_delta_time(data)
@@ -47,13 +59,22 @@ def main():
             print(time_format)
             # faire tout le reste le temps a été split
 
+        # FINAL TIME
         client.send_data(getfinaltime_comp)
         data = client.receive_data()
         unique_final_time = get_unique_final_time(data)
 
         if unique_final_time:
-            time_formats = convert_time_format(unique_final_time)
-            print(time_formats)
+            time_format = convert_time_format(unique_final_time)
+            print(time_format)
+
+        # RESET
+        client.send_data(gettimerphase)
+        data = client.receive_data()
+        is_reset = get_unique_timer_phase(data)
+
+        if is_reset:
+            print("-")
 
 
 if __name__ == '__main__':
