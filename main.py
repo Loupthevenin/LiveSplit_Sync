430cbc06e7b23454ff815d2322306dbc8dfa5b45
AGENT MPR 2 <agent.mpr2@gd.local>
2024-03-15 11:50:25 +0100

    New list

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     waiting = False
     save_time_format = ""
 
+    time_list = []
+    total_splits = how_many_split()
+
     client = Connect(SERVER_IP, PORT)
     client.connect()
 
@@ -32,8 +35,10 @@ def main():
             if split_index:
                 split_index = int(split_index) + nb_cols_before_split_sheets
 
-                if split_index == current_split():
+                # if split_index == current_split():
+                if split_index == len(time_list) + nb_cols_before_split_sheets + 1:
                     write_time(time_format, split_index)
+                    time_list.append(time_format)
                 else:
                     print("Decalage avec le sheets")
 
@@ -43,19 +48,24 @@ def main():
             time_format = convert_time_format(final_time)
             save_time_format = time_format
             print(time_format)
-            total_split = (current_split() - nb_cols_before_split_sheets - 1)
+            # total_split = (current_split() - nb_cols_before_split_sheets - 1)
+            total_split = len(time_list)
 
-            if total_split == how_many_split():
-                split_index = current_split()
+            if total_split == total_splits:
+                # split_index = current_split()
+                split_index = len(time_list) + nb_cols_before_split_sheets + 1
                 write_time(save_time_format, split_index)
+                time_list.append(time_format)
                 new_row()
-            elif total_split < how_many_split():
+                time_list.clear()
+            elif total_split < total_splits:
                 waiting = True
                 continue
         elif waiting:
-            total_split = (current_split() - nb_cols_before_split_sheets - 1)
-            if total_split == how_many_split():
-                split_index = current_split()
+            total_split = len(time_list)
+            if total_split == total_splits:
+                # split_index = current_split()
+                split_index = len(time_list) + nb_cols_before_split_sheets + 1
                 write_time(save_time_format, split_index)
                 new_row()
                 waiting = False
@@ -68,6 +78,7 @@ def main():
                 if split_index == '-1':
                     write_reset()
                     new_row()
+                    time_list.clear()
 
 
 if __name__ == '__main__':
