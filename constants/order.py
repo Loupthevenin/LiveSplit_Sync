902b8b361a2c76d469eb697c8d71b273d0b81897
AGENT MPR 2 <agent.mpr2@gd.local>
2024-04-03 15:08:33 +0200

    add order

--- constants/order.py
+++ constants/order.py
@@ -3,3 +3,6 @@ getfinaltime_comp = "getfinaltime COMPARISON\r\n"
 getsplitindex = "getsplitindex\r\n"
 getcurrentrealtime = "getcurrentrealtime\r\n"
 gettimerphase = "getcurrenttimerphase\r\n"
+
+# TESTER LE PREVIOUS TIME
+getprevioussplittime = "getprevioussplittime\r\n"
